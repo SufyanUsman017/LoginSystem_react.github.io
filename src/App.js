c14a9783c785d9a2df9e40e3a095337a0a9b8a8b
SufyanUsman017 <162179435+SufyanUsman017@users.noreply.github.com>
2024-10-20 12:31:37 +0500

    LoginSystem_React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState,CSSProperties } from 'react'
+import './App.css'
+import SignUp from './components/SignUp'
+import ClipLoader from "react-spinners/ClipLoader";
 
-function App() {
+
+
+
+// const override: CSSProperties = {
+//   display: "block",
+//   margin: "0 auto",
+//   borderColor: "green",
+// };
+
+
+
+export default function App() {
+
+
+  // const [num ,setNum ] = useState(0);
+  // const [num1 ,setNum1 ] = useState(0);
+  // const [loading, setLoading] = useState(true);
+  // const [color, setColor] = useState("#ffffff");
+
+  
+
+  // useEffect(()=>{setTimeout(()=>{setLoading(false)},3000)}, [num])
+
+  // function Inc(){
+  //   setNum(num+1)
+  //   setLoading(true);
+  // }
+  // function Dec(){
+  //   setNum(num-1)
+  //   setLoading(true);
+  // }
+
+  // function Inc1(){
+  //   setNum1(num1+1)
+  //   setLoading(true);
+  // }
+  // function Dec1 (){
+  //   setNum1(num1-1)
+  //   setLoading(true);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      
+      <SignUp />
 
-export default App;
+{/* 
+      <center>
+      <button onClick={Dec} > -</button>
+        <h1>{num}</h1>
+        <button onClick={Inc} > +</button>
+      </center>
+
+<br/>
+<br/>
+
+
+      <center>
+      <button onClick={Dec1} > -</button>
+        <h1>{num1}</h1>
+        <button onClick={Inc1} > +</button>
+      </center>
+
+      <div className="sweet-loading">
+     
+
+      <ClipLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={100}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div> */}
+      
+
+    </div>
+    
+  )
+}
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+
+export default function SignUp() {
+  const [users, setUsers] = useState([]);
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+  const [inputPassword, setInputPassword] = useState("");
+  const [personName, setPersonName] = useState("");
+
+  function signUp(e) {
+    e.preventDefault();
+    const userFound = users.find((user) => user.userEmail === userEmail);
+    if (!userFound) {
+      setUsers([...users, { userName, userEmail, userPassword }]);
+      alert("User registered successfully");
+    } else {
+      alert("User already exists");
+    }
+    setUserName("");
+    setUserEmail("");
+    setUserPassword("");
+  }
+
+  function signIn(e) {
+    e.preventDefault();
+    const userFound = users.find((user) => user.userEmail === inputEmail && user.userPassword === inputPassword);
+    if (userFound) {
+      alert("Welcome");
+      setPersonName(userFound.userName);
+    } else {
+      alert("Invalid email or password");
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="signup-container">
+        <h2>Sign Up</h2>
+        <form onSubmit={signUp}>
+          <label htmlFor="userName">Name:</label>
+          <input
+            type="text"
+            name="userName"
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            required
+          />
+          <label htmlFor="userEmail">Email:</label>
+          <input
+            type="email"
+            name="userEmail"
+            onChange={(e) => setUserEmail(e.target.value)}
+            value={userEmail}
+            required
+          />
+          <label htmlFor="userPassword">Password:</label>
+          <input
+            type="password"
+            name="userPassword"
+            onChange={(e) => setUserPassword(e.target.value)}
+            value={userPassword}
+            required
+          />
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+
+      <div className="signin-container">
+        <h2>Sign In</h2>
+        <form onSubmit={signIn}>
+          <label htmlFor="inputEmail">Email:</label>
+          <input
+            type="email"
+            name="inputEmail"
+            onChange={(e) => setInputEmail(e.target.value)}
+            value={inputEmail}
+            required
+          />
+          <label htmlFor="inputPassword">Password:</label>
+          <input
+            type="password"
+            name="inputPassword"
+            onChange={(e) => setInputPassword(e.target.value)}
+            value={inputPassword}
+            required
+          />
+          <button type="submit">Sign In</button>
+        </form>
+        <center>
+          <p>Mr. {personName} Welcome...</p>
+        </center>
+      </div>
+
+      <div className="items-list">
+        {users.map((user, index) => (
+          <div key={index}>
+            <h2>{index + 1}</h2>
+            <p>Name: {user.userName}</p>
+            <p>Email: {user.userEmail}</p>
+            <p>Password: {user.userPassword}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
